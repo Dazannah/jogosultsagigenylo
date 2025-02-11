9edb881ed3dd17fcc68c15937a41e81a8c121a3c
Dazannah <beattius@gmail.com>
2025-02-11 20:47:01 +0100

    position migration added

--- jogosultsagigenylo.Server/Migrations/20250208145419_Added position to column.Designer.cs
+++ jogosultsagigenylo.Server/Migrations/20250208145419_Added position to column.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using jogosultsagigenylo.Server.Data;
 
@@ -10,9 +11,11 @@ using jogosultsagigenylo.Server.Data;
 namespace jogosultsagigenylo.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250208145419_Added position to column")]
+    partial class Addedpositiontocolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ jogosultsagigenylo.Server/Migrations/20250208145419_Added position to column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jogosultsagigenylo.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedpositiontocolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "Columns",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Columns");
+        }
+    }
+}
