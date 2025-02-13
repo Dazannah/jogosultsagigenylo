078cbe6cddd555177b5dfd77e49147c88e3bb3ca
Dazannah <beattius@gmail.com>
2025-02-13 20:33:55 +0100

    created departmentDTO

---
+++ jogosultsagigenylo.Server/DTO/DepartmentDTO.cs
@@ -0,0 +1,21 @@
+﻿using jogosultsagigenylo.Server.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jogosultsagigenylo.Server.DTO {
+	public class DepartmentDTO {
+		[Key]
+		public int Id { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Atonosító nem megfelelő.")]
+		public required string ClassNumber { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Elnevezés nem megfelelő.")]
+		public required string DisplayName { get; set; }
+		[Range(1, int.MaxValue, ErrorMessage = "Hibás helyszín id.")]
+		public required int LocationId { get; set; }
+		[ForeignKey("LocationId")]
+		public Location? Location { get; set; }
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Kategória nem megfelelő.")]
+		public required string Category { get; set; }
+	}
+}
+
