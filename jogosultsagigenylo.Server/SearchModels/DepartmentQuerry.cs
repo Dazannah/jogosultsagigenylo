aa8d7c4c722ffc8dc3f08a79c40f05a4d8fe1e41
Dazannah <beattius@gmail.com>
2025-02-20 20:53:10 +0100

    deleted FromQuery annotation

--- jogosultsagigenylo.Server/SearchModels/DepartmentQuerry.cs
+++ jogosultsagigenylo.Server/SearchModels/DepartmentQuerry.cs
@@ -1,15 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace jogosultsagigenylo.Server.SearchModels {
+﻿namespace jogosultsagigenylo.Server.SearchModels {
 	public class DepartmentQuerry {
 		public int? Id { get; set; }
 		public string? DepartmentNumber { get; set; }
 		public string? DisplayName { get; set; }
 		public int? LocationId { get; set; }
 		public int? CategoryId { get; set; }
-		[FromQuery(Name = "Page")]
 		public int Page { get; set; } = 1;
 		public int ItemsOnPage { get; set; } = 10;
-
 	}
 }
