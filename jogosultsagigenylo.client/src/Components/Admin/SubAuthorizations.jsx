6ccf30b3ccbdcddb1bf87d890a0ffb452762dea2
Dazannah <beattius@gmail.com>
2025-02-23 18:33:02 +0100

    subauthorization base added

--- jogosultsagigenylo.client/src/Components/Admin/SubAuthorizations.jsx
+++ jogosultsagigenylo.client/src/Components/Admin/SubAuthorizations.jsx
@@ -1,6 +1,46 @@
+﻿import React from "react";
+import { useEffect, useState } from "react";
+
+import Container from "../Container";
+
+import { AppContext, DispatchContext } from "../../main";
+import Loading from "../Loading";
 function SubAuthorizations() {
+    const dispatch = React.useContext(DispatchContext);
+
+    const title = "Aljogosultságok"
+    const [authItems, setAuthItems] = useState([]);
+    const [subAuthItems, setSubAuthItems] = useState([]);
+    const [isLoading, setIsloading] = useState(true);
+
+    useEffect(() => {
+        if (isLoading) {
+            fetch("/api/subauthitems")
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    setAuthItems(data.authItems)
+                    setSubAuthItems(data.subAuthItems)
+
+                    setIsloading(false)
+                })
+                .catch(error => {
+                    dispatch({ type: "flashMessageError", action: { message: error.errors } });
+                })
+        }
+    }, [isLoading])
+
+    if (isLoading) return (
+        <Container title={title}>
+            <Loading></Loading>
+        </Container>
+    )
+
   return (
-    <p>Hello world!</p>
+      <Container title={title}>
+          <Loading></Loading>
+      </Container>
   );
 }
 
