86463c8ccbd4b9ae75dd1ad13c6b91dce1a62a41
Dazannah <beattius@gmail.com>
2025-02-28 20:56:02 +0100

    Update and UpdateOrder added

--- jogosultsagigenylo.Server/Services/AuthItemService.cs
+++ jogosultsagigenylo.Server/Services/AuthItemService.cs
@@ -5,7 +5,7 @@ using jogosultsagigenylo.Server.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace jogosultsagigenylo.Server.Services {
-	public class AuthItemService : IAuthItem {
+	public class AuthItemService : IAuthItemService {
 
 		private readonly ApplicationDbContext _context;
 
@@ -17,7 +17,7 @@ namespace jogosultsagigenylo.Server.Services {
 		/// Check if IDs exist and create the auth item.
 		/// </summary>
 		/// <exception cref="KeyNotFoundException">Thrown when authItemDTO ColumnId or StatusId is not found in DB.</exception>
-		public async Task<bool> CreateOne(AuthItemDTO authItemDTO) {
+		public async Task<bool> CreateOne(IAuthItemDTO authItemDTO) {
 			var column = await _context.Columns.FirstOrDefaultAsync(c => c.Id == authItemDTO.ColumnId)
 				?? throw new KeyNotFoundException($"Oszlop {authItemDTO.ColumnId} id-val nem található.");
 
@@ -45,5 +45,47 @@ namespace jogosultsagigenylo.Server.Services {
 
 			return true;
 		}
+
+		public async Task<bool> Update(int id, IAuthItemDTO authItemDTO) {
+			var authItem = await _context.AuthItems
+				.FirstOrDefaultAsync(ai => ai.Id == id)
+				?? throw new KeyNotFoundException($"Jogosultság {id} id-val nem található.");
+
+			authItem.DisplayName = authItemDTO.DisplayName;
+			authItem.StatusId = authItemDTO.StatusId;
+			authItem.ColumnId = authItemDTO.ColumnId;
+			authItem.Position = authItemDTO.Position;
+
+			_context.AuthItems.Update(authItem);
+			var updated = await _context.SaveChangesAsync();
+
+			if(updated < 1)
+				return false;
+
+			return true;
+		}
+
+		public async Task<int> UpdateOrder(int columnId, List<AuthItemDTO> authItemDTOs) {
+			var authItems = _context.AuthItems.Where(a => a.ColumnId == columnId)
+				?? throw new KeyNotFoundException($"Oszlop {columnId} id-val nem található.");
+
+			if(authItems != null) {
+				var positions = new Dictionary<int, int>();
+
+				foreach(var authItemDTO in authItemDTOs) {
+					positions.Add(authItemDTO.Id, authItemDTO.Position);
+				}
+
+				foreach(var authItem in authItems) {
+					authItem.Position = positions[authItem.Id];
+				}
+
+				_context.AuthItems.UpdateRange(authItems);
+				return await _context.SaveChangesAsync();
+			}
+
+			return 0;
+		}
 	}
 }
+
