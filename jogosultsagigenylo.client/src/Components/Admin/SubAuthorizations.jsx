df1ecb0517b9443e8a0b2cdc1200147b53fb7a19
Dazannah <beattius@gmail.com>
2025-04-01 18:44:39 +0200

    sub auth Item list base added

--- jogosultsagigenylo.client/src/Components/Admin/SubAuthorizations.jsx
+++ jogosultsagigenylo.client/src/Components/Admin/SubAuthorizations.jsx
@@ -1,6 +1,25 @@
 ﻿import React from "react";
 import { useEffect, useState } from "react";
 
+import {
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    useSensor,
+    useSensors,
+    DragOverlay,
+    useDndMonitor
+} from '@dnd-kit/core';
+
+import { MyPointerSensor } from "../../Classes/MyPointerSensor"
+
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+    horizontalListSortingStrategy,
+} from '@dnd-kit/sortable';
+
 import Container from "../Container";
 
 import { AppContext, DispatchContext } from "../../main";
@@ -11,6 +30,56 @@ function SubAuthorizations() {
     const title = "Aljogosultságok"
     const [authItems, setAuthItems] = useState([]);
     const [isLoading, setIsloading] = useState(true);
+
+    const [activeItem, setActiveItem] = useState(null)
+
+    const sensors = useSensors(
+        useSensor(MyPointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    )
+
+    async function handleDragEnd(event) {
+        const { active, over } = event;
+        const activeId = +active.id.split("-")[0]
+        const overId = +over.id.split("-")[0]
+        if (active.id !== over.id && +active.id.split("-")[2] === +over.id.split("-")[2]) {
+            const columnId = +active.id.split("-")[2]
+            let columnIdx
+
+            let oldIndex = -1
+            let newIndex = -1
+
+            let tempColumns = columns
+
+            const newColumns = tempColumns.map((column, cIdx) => {
+                if (column.id === columnId) {
+                    columnIdx = cIdx
+
+                    column.authItems.map((authItem, idx) => {
+                        if (authItem.id === activeId) oldIndex = idx
+                        if (authItem.id === overId) newIndex = idx
+                    })
+
+                    column.authItems = arrayMove(column.authItems, oldIndex, newIndex)
+
+                    column.authItems.forEach((authItem, aIdx) => {
+                        authItem.position = aIdx + 1
+                    })
+                }
+
+                return column
+            })
+
+            const authItems = columns[columnIdx].authItems
+
+            await updateAuthItemOrder(authItems, columnId)
+            setColumns(newColumns);
+        }
+
+
+    }
 
     useEffect(() => {
         if (isLoading) {
@@ -30,20 +99,19 @@ function SubAuthorizations() {
     }, [isLoading])
 
     function renderSubAuthItems(authItem) {
-        return authItem.subAuthItems.map((subAuthItem, idx) => {
+        return authItem.subAuthItems.map(subAuthItem => {
             return (
-                <span key={`subAuthItem-span-${idx}`}>{subAuthItem}</span>
+                <span key={`subAuthItem-span-${subAuthItem.subAuthItemId}`}>{subAuthItem.id} {subAuthItem.displayName} {subAuthItem.subAuthItemId}</span>
             )
         })
     }
 
     function renderAuthItems(){
-        return authItems.map((authItem, idx) => {
-            console.log(authItem, idx)
+        return authItems.map(authItem => {
+            console.log(authItem)
             return (
-                <div key={`authItem-div-${idx}`}>
-                    <span key={`authItem-span-${idx}`}>{authItem.displayName} items: </span>
-
+                <div key={`authItem-div-${authItem.id}`}>
+                    <span key={`authItem-span-${authItem.id}`}>{authItem.displayName} items: </span>
                     {
                         renderSubAuthItems(authItem)
                     }
@@ -59,15 +127,45 @@ function SubAuthorizations() {
         </Container>
     )
 
-  return (
-      <Container title={title}>
+    return (
+        <Container title={title}>
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd} >
+                <div className="flex flex-wrap">
+
+
+                    {authItems.map(column => {
+                        return (
+                            "asd"
+                            //<AdminColumn id={`${column.id}-div`} key={`admin-column-key-${column.id}`} column={column} statuses={statuses} columns={columns} setIsLoading={setIsLoading} setActiveItem={setActiveItem} />
+
+                        );
+                    })}
+
+
+                    <DragOverlay >
+                        {activeItem ?
+                            <table className="w-full">
+                                <tbody>
+                                    <AdminAuthItem id={`${activeItem.id}-tr`} key={`admin-authItem-key-${activeItem.id}`} authItem={activeItem} columnId={activeItem.id} showDiv={() => { }} grabbing={true} />
+                                </tbody>
+                            </table>
+                            : null}
+                    </DragOverlay>
+                </div>
+            </DndContext>
+        </Container>
+    )
+  //return (
+  //    <Container title={title}>
           
-          {
-              renderAuthItems()
-          }
           
-      </Container>
-  );
+  //            <>
+  //                {renderAuthItems()}
+  //            </>
+          
+          
+  //    </Container>
+  //);
 }
 
 export default SubAuthorizations;
